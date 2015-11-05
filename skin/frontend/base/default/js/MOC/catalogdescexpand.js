36a8bef44cdb08e7fd25eb40bb514aa7bbd0bf21
tonioMtl <tonio.cz@gmail.com>
2015-11-05 18:24:34 +0100

    js

--- skin/frontend/base/default/js/MOC/catalogdescexpand.js
+++ skin/frontend/base/default/js/MOC/catalogdescexpand.js
@@ -1,5 +1,7 @@
 (function( $ ) {
 
+	$('body.catalog-category-view .main .col-main h1').next().addClass('expandable');
+
 	$('div.expandable').expander({
 		expandPrefix: '&#8230; ',
 		expandText: 'Lire la suite',
