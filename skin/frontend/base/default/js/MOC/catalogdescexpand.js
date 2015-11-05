83eb377c9975703707b6441c5ace91c1f66c547e
tonioMtl <tonio.cz@gmail.com>
2015-11-05 18:32:26 +0100

    js

--- skin/frontend/base/default/js/MOC/catalogdescexpand.js
+++ skin/frontend/base/default/js/MOC/catalogdescexpand.js
@@ -1,10 +1,12 @@
 (function( $ ) {
 
-	$('div.expandable').expander({
-		expandPrefix: '&#8230; ',
-		expandText: 'Lire la suite',
-		userCollapseText: 'Fermer',
-		userCollapsePrefix: ' '
+	$(function() {
+		$('div.expandable').expander({
+			expandPrefix: '&#8230; ',
+			expandText: 'Lire la suite',
+			userCollapseText: 'Fermer',
+			userCollapsePrefix: ' '
+		});
 	});
 
 }( jQuery ));
